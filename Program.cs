119bac699cf43275960056ccaeb36a27336b9666
KevinMarin <kevinmarin2005@gmail.com>
2021-02-09 20:28:51 -0600

    Llame a mi proyecto desde el Main

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ namespace PruebaCurso
             Console.WriteLine("Ejercicio de Andony: ");
             var suma = new SumaNumeros();
             Console.WriteLine(suma.Sum(1, 2));
-
+            Console.WriteLine("Ejercicio de Kevin: ")
+            Console.WriteLine(AgeToDays.CalcEdad(15));
         }
     }
 }
