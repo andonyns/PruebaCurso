d20143da5f3d8292be36aca579eed2ce1a98ea13
KevinMarin <kevinmarin2005@gmail.com>
2021-02-09 20:20:32 -0600

    Añadir mi proyecto

---
+++ AgeToDays.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PruebaCurso
+{
+    class AgeToDays
+    {
+        public static int CalcEdad(int CalcEdad) 
+        {
+                return CalcEdad * 365;
+        }
+    }
+}
