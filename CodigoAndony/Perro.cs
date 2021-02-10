76e50c179ecfc93c87add0e75af4f0af704bdf13
Andony Nuñez <andony91@gmail.com>
2021-02-09 18:46:01 -0600

    Agregando animal y perro

---
+++ CodigoAndony/Perro.cs
@@ -0,0 +1,36 @@
+
+using System;
+
+namespace Ejemplo
+{
+    class Perro : Animal
+    {
+        private int edad;
+        public Perro(string nuevoNombre, int nuevaEdad)
+        {
+            nombre = nuevoNombre;
+            edad = nuevaEdad;
+        }
+
+        public Perro()
+        {
+
+        }
+
+        public override string EmitirSonido()
+        {
+            return "guau. Mi nombre es: " + _nombre;
+        }
+
+        public string TenerHijos()
+        {
+            return "viviparo";
+        }
+
+        public void AlgoQueSoloElPerroHace()
+        {
+            //no hace nada
+        }
+
+    }
+}
