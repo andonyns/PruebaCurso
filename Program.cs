8039dd5117c23d32a48ecc5bf934e7b65620885d
Jeremy Briones <jeremybr99@gmail.com>
2021-02-09 21:00:34 -0600

    cambio jeremy

--- Program.cs
+++ Program.cs
@@ -19,16 +19,13 @@ namespace PruebaCurso
             var area = new areaTriandulo();
             Console.WriteLine(area.triArea(12, 32));
 
-<<<<<<< HEAD
             Console.WriteLine("Ejercicio de Jeremy: ");
             var contarNum = new Conteo();
             contarNum.Contar();
-=======
 
             Console.WriteLine("Ejercicio de Kimberly: ");
             var multi = new MultiplicacionNumeros();
             Console.WriteLine(multi.Mult(5, 7));
->>>>>>> 64f4c18f77255ee1a8846816fc5251ef2c65db96
 
         }
     }
